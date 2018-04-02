8d15741c59b2ab5e2fc54a0c23e72890d19876d0
vwtt <vikram.srirangam@outlook.com>
2018-04-02 07:40:13 +0530

    Initial Commit.

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+const PrimaryLayout = () => (
+  <div className="primary-layout">
+    <header>
+      Our React Router 4 App
+    </header>
+    <Link to="/users">Users</Link>
+    <main>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/users" component={UsersPage} />
+    </main>
+  </div>
+)
+
+const HomePage =() => <div>Home Page</div>
+const UsersPage = () => <div>Users Page</div>
+
+const App = () => (
+  <BrowserRouter>
+    <PrimaryLayout />
+  </BrowserRouter>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
+module.hot.accept();
